7ef9dca47371dc2f5e32c0b9ed125ab2fafba938
amrmohamed <am1066903@gmail.com>
2022-01-26 16:01:57 +0200

    fix firbase

---
+++ build/precache-manifest.dabd9900f12e12d79c0a6e76da94a6b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae837e3fa54a3eb4b97ee26f0865acc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a273f3ee182ea76f963c",
+    "url": "/static/css/main.8b421d16.chunk.css"
+  },
+  {
+    "revision": "bcbaf69e87362736ec6f",
+    "url": "/static/js/2.8fd13db8.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/static/js/2.8fd13db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a273f3ee182ea76f963c",
+    "url": "/static/js/main.6175bf81.chunk.js"
+  },
+  {
+    "revision": "723b8de4144d7b4aea6d",
+    "url": "/static/js/runtime-main.562f75cf.js"
+  }
+]);
